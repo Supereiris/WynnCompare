0fbf5db686b64b7b965c4fa0a0fee7c2c7bce22b
Supereiris <ircostantinou@gmail.com>
2026-02-23 13:33:33 +0200

    added keybind settings and icon

--- src/main/java/com/wynncompare/WynnCompareClient.java
+++ src/main/java/com/wynncompare/WynnCompareClient.java
@@ -7,6 +7,7 @@ import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,9 @@ public class WynnCompareClient implements ClientModInitializer {
 
     public static final String MOD_ID = "wynncompare";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    private static final KeyBinding.Category KEYBIND_CATEGORY =
+            KeyBinding.Category.create(Identifier.of(MOD_ID, "keybinds"));
 
     public static KeyBinding compareKey;
     public static KeyBinding detailCompareKey;
@@ -25,14 +29,14 @@ public class WynnCompareClient implements ClientModInitializer {
                 "key.wynncompare.compare",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_C,
-                KeyBinding.Category.MISC
+                KEYBIND_CATEGORY
         ));
 
         detailCompareKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.wynncompare.detail_compare",
                 InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_X,
-                KeyBinding.Category.MISC
+                KEYBIND_CATEGORY
         ));
 
         ScreenEvents.BEFORE_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
